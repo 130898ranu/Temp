06a1710160790693f13cd71b14dcba7465f6d12a
130898ranu <salunkhesunita98@gmail.com>
2023-05-16 14:42:14 +0530

    Added:By Some Program

---
+++ src/com/bridglabz/PracticePrograme/EmployeeWageBuilderUC1.java
@@ -0,0 +1,13 @@
+package com.bridglabz.PracticePrograme;
+
+public class EmployeeWageBuilderUC1 {
+    public static void main(String[] args) {
+
+        System.out.println();
+        int n1=20, n2=30;
+        int Sum=n1+n2;
+        System.out.println(Sum);
+    }
+
+}
+
